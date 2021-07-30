6b944118a352ba06193166c9186f55bd8429ca00
luzelenie <luzelenie1205@gmail.com>
2021-07-30 13:53:56 -0400

    commit tree 1448fe3eb3137c9b508de38d93a480b8edd12416

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(say="roller-skate"){
+return `This Saturday, I want to ${say}!`
+}
+
+function mondayWork(say='go to the office'){
+return `This Monday, I will ${say}.`
+}   
+
+function wrapAdjective(say1='*'){
+     
+   return function (str="a hard woker"){
+    return `You are ${say1}${str}${say1}!`
+}
+}
+wrapAdjective(("||")("a dedicated programmer"))
